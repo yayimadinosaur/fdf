a52c70cbf083e1aabfa11c5d7e9a49719a935da0
warren fung <wfung@e1z2r6p1.42.us.org>
2017-04-22 17:35:21 -0700

    push in case

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/21 19:34:08 by wfung             #+#    #+#             */
-/*   Updated: 2017/04/21 21:33:45 by wfung            ###   ########.fr       */
+/*   Updated: 2017/04/22 17:33:34 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "mlx.h"
 #include <unistd.h>
 
+//	42 logo star wars color w/ dots at variable + 5
 typedef struct	s_env
 {
 	void *mlx;
@@ -34,7 +35,7 @@ int		draw_mouse(void *mlx, void *win, int button, int x, int y)
 	else if (button == 2)
 	{
 	//	if (x > 100 && x < 200)
-			mlx_pixel_put(mlx, win, x, y, 0x00FFFF);
+			mlx_pixel_put(mlx, win, x, y, 0x00AAAF);
 	//	else
 	//		mlx_pixel_put(e->mlx, e->win, x, y, 0x0000011);
 	}
@@ -47,16 +48,37 @@ void	draw(void *mlx, void *win)
 	int		x;
 	int		y;
 
-	y = 100;
-	while (y < 300)
+	y = 50;
+	while (y < 301)
 	{
-		x = 100;
-		while (x < 300)
+		x = 50;
+		while (x < 501)
 		{
-			mlx_pixel_put(mlx, win, x, y, 0x00FFFFF);
-			x = x + 7;
+//			if (x == 50 || y == 50 || x == 299 || y == 299)
+//				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);	//border hardcoded
+			if (x % 25 == 0 || y % 25 == 0)			//border w %
+				mlx_pixel_put(mlx, win, x, y, 0xCCCC00D); //white
+			if (x >= 100 && x <= 150 && y >= 100 && y <= 200) 
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 200 && x <= 250 && y >= 100 && y <= 275)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 150 && x <= 200 && y >= 175 && y < 225)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 125 && x <= 150 && y >= 200 && y < 225)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 300 && x <= 425 && y >= 100 && y < 125)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 325 && x <= 425 && y >= 175 && y <= 200)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 300 && x <= 450 && y >= 250 && y <= 275)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 400 && x <= 450 && y >= 125 && y <= 175)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			if (x >= 300 && x <= 350 && y >= 200 && y <= 250)
+				mlx_pixel_put(mlx, win, x, y, 0xAAAAAAA);
+			x = x + 1;
 		}
-		y = y + 7;
+		y = y + 1;
 	}
 }
 
@@ -85,7 +107,7 @@ int		main(int ac, char **av)
 	t_env	e;
 
 	e.mlx = mlx_init();
-	e.win = mlx_new_window(e.mlx, 1000, 1000, "42");
+	e.win = mlx_new_window(e.mlx, 550, 400, "42");
 	draw(e.mlx, e.win);
 	mlx_key_hook(e.win, key_hook, &e);
 //	if (mlx_key_hook(e.win, key_hook, &e) == 53)
@@ -94,7 +116,7 @@ int		main(int ac, char **av)
 	mlx_mouse_hook(e.win, mouse_hook, &e);
 //	mlx_mouse_hook(e.win, draw_mouse, &e);
 	mlx_loop(e.mlx);
-	//sleep(5);
-	//usleep(5000);
+	sleep(5);
+	usleep(5000);
 	return (0);
 }
